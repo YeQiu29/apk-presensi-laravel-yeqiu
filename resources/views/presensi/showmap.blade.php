ace46654d3a411a24be3932c0109c66ba0748508
Dennis Hilmansyah <denniez.hilmansyah29@gmail.com>
2025-12-09 16:19:59 +0700

    showmap update 5

--- resources/views/presensi/showmap.blade.php
+++ resources/views/presensi/showmap.blade.php
@@ -3,14 +3,6 @@
         height: 250px; 
         width: 100%;
     }
-
-    /* CSS untuk membuat Titik Lokasi User yang cantik (Google Maps Style) */
-    .gps-point {
-        background-color: #3388ff; /* Warna Biru Leaflet */
-        border: 2px solid white;   /* Garis tepi putih */
-        border-radius: 50%;        /* Membuatnya bulat sempurna */
-        box-shadow: 0 0 8px rgba(0,0,0,0.4); /* Efek bayangan agar timbul */
-    }
 </style>
 
 <div id="map"></div>
@@ -30,38 +22,14 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    // 3. MEMBUAT TITIK LOKASI USER (Gaya Google Maps)
-    // Kita gunakan DivIcon agar bisa dikasih Style CSS (.gps-point)
-    var iconTitik = L.divIcon({
-        className: 'gps-point', // Panggil class CSS di atas
-        iconSize: [16, 16],     // Ukuran titik (px)
-        iconAnchor: [8, 8]      // Titik jangkar pas di tengah (setengah dari ukuran)
-    });
+    // 3. TAMPILKAN MARKER (PIN) LANGSUNG
+    // Marker langsung ditambahkan ke peta saat dimuat.
+    // .bindPopup() akan membuat popup nama muncul otomatis HANYA saat marker diklik.
+    var marker = L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("{{ $presensi->nama_lengkap }}");
 
-    // Tampilkan titiknya di peta
-    var markerTitik = L.marker([latitude, longitude], {
-        icon: iconTitik
-    }).addTo(map);
-
-
-    // 4. Logic: Klik Titik -> Munculkan Marker Pin Besar
-    var markerPin = null; 
-
-    markerTitik.on('click', function() {
-        if (markerPin) {
-            // Jika pin sudah ada, hapus (toggle)
-            map.removeLayer(markerPin);
-            markerPin = null;
-        } else {
-            // Jika belum ada, munculkan Pin Marker standar + Popup Nama
-            markerPin = L.marker([latitude, longitude])
-                .addTo(map)
-                .bindPopup("{{ $presensi->nama_lengkap }}")
-                .openPopup();
-        }
-    });
-
-    // 5. Tampilkan Radius Kantor (Lingkaran Merah)
+    // 4. Tampilkan Radius Kantor (Lingkaran Merah)
     var lokasi_kantor = "{{ $lokasi_kantor->lokasi_kantor }}";
     var lok_kantor = lokasi_kantor.split(",");
     var lat_kantor = lok_kantor[0];
@@ -75,10 +43,11 @@
         radius: radius
     }).addTo(map);
 
-    // 6. Fix Bug Peta Abu-abu & Center
+    // 5. Fix Bug Peta Abu-abu (PENTING)
+    // Tetap diperlukan karena peta ada di dalam Modal
     setTimeout(function() {
         map.invalidateSize();
-        map.panTo([latitude, longitude]);
+        map.panTo([latitude, longitude]); // Geser kamera pas ke marker
     }, 800);
 
 </script>
